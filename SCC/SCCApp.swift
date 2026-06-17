c43dfcd096b553b93ba983590e8cb778ee42a9aa
edilayehu <edilayehu@eaglelionsystems.com>
2026-06-17 10:39:26 +0300

    Initial Commit

---
+++ SCC/SCCApp.swift
@@ -0,0 +1,17 @@
+//
+//  SCCApp.swift
+//  SCC
+//
+//  Created by Edil on 17/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct SCCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
